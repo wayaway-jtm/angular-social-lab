fcc27f8f7bbaf10c2c6651023be509f2f491bfe2
john <wayaway.jtm@gmail.com>
2020-01-22 18:39:00 -0500

    generated necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialPostsComponent } from './social-posts/social-posts.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialPostsComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/social-posts/social-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css']
+})
+export class SocialPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
